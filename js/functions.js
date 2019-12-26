97d70aeb5070f6c8b635b2d8717bb9b84ba94886
Paula211 <59137080+Paula211@users.noreply.github.com>
2019-12-26 12:52:23 +0100

    Update functions.js

---
+++ js/functions.js
@@ -0,0 +1,14 @@
+var time= new Date();
+var color;
+var myElement;
+if(time == 12) {
+    color = "rojo";
+}
+else if(time == 15) {
+    color = "amarillo";
+}
+else{
+    color = "verde";
+}
+var myElement = document.getElementById("timeColor");
+myElement.innerHTML = color;
